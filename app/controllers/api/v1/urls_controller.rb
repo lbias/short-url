b52ff95c40d4b0fb7030848bae51c4c44e4376a7
lbias <306508354@qq.com>
2017-08-04 20:11:11 +0800

    controller api, api::v1::urls#create

---
+++ app/controllers/api/v1/urls_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UrlsController < ApiController
+  def create
+    @url = current_user.urls.new(url_params)
+    if @url.save
+      render json: @url, status: :created
+    else
+      render json: @url.errors, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original_url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :urls
+    end
+  end
 end
